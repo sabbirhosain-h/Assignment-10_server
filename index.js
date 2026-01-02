fb1fbab8ac597ef075729b4474370b163b306948
Sabbir Hosain <sabbirhosain298+github@gmail.com>
2026-01-03 04:10:55 +0600

    Book details api made

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;  
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // require('dotenv').config();
 
@@ -30,7 +30,31 @@ async function run() {
         const db = client.db("bookHeaven");
         const booksCollection = db.collection("books");
 
+
+
+
+
+        // ALL Books
+        app.get("/AllBooks" , async (req, res) => {
+            const cursor = booksCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
         
+        
+        // get book data
+        app.post("/AllBooks" , async (req, res) =>{
+            const newBook = req.body;
+            const result = await booksCollection.insertOne(newBook);
+            res.send(result);
+        })
+        // Ger single book
+        app.get("/BookDetails/:id" , async (req,res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await booksCollection.findOne(query)
+            res.send(result);
+        })
         
     } finally{
 
